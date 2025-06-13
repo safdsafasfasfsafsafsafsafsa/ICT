80b5055f1d9e7407bf194a0a922896c6a12487eb
asd <poo1oox@outlook.com>
2025-06-13 15:39:59 +0900

    spreadsheet 수정 중

--- mon/5/script.js
+++ mon/5/script.js
@@ -49,15 +49,15 @@ class Cell {
     this.col = col;
     this.rowName = rowName;
     this.colName = colName;
-    this.data = data;
-    this.isHeader = isHeader;
-    this.disabled = disabled;
+    this.data = data; // 데이터
+    this.isHeader = isHeader; // 헤더 여부
+    this.disabled = disabled; // 수정 가능 여부
     this.active = active;
   }
 }
 
 function initSpreadsheet() {
-  // html 투입
+  // html 투입: 행렬 길이만큼 순회
   for (let i = 0; i < ROWS; i++) {
     // 클래스 cellRow 추가: cell 담기
     const cellRowEl = document.createElement("div");
@@ -83,7 +83,7 @@ function initSpreadsheet() {
         j,
         i,
         alphabets[j - 1],
-        "",
+        i + "-" + j, // 위치 출력에 쓸 데이터
         isHeader,
         disabled,
         false
@@ -91,6 +91,7 @@ function initSpreadsheet() {
       spreadsheetRow.push(newCell);
 
       // 클래스 cell 추가
+      // createCellEl(cellRowEl);
       const cellEl = document.createElement("div");
       cellEl.classList.add("cell");
       cellRowEl.appendChild(cellEl);
@@ -107,7 +108,7 @@ function initSpreadsheet() {
           spanEl.textContent = ``;
         }
       }
-      // spanEl.textContent = `${i}-${alphabets[j - 1]}`;
+
       cellEl.appendChild(spanEl);
     }
     spreadsheet.push(spreadsheetRow);
@@ -115,15 +116,38 @@ function initSpreadsheet() {
   console.log(spreadsheet);
 }
 
+function createCellEl(cellRowEl) {
+  const cellEl = document.createElement("div");
+  cellEl.classList.add("cell");
+  cellRowEl.appendChild(cellEl);
+
+  const spanEl = document.createElement("span");
+  spanEl.classList.add("spanText");
+  if (i === 0) {
+    if (j === 0) {
+      spanEl.textContent = ``;
+    } else {
+      spanEl.textContent = `${alphabets[j - 1]}`;
+    }
+  } else {
+    if (j === 0) {
+      spanEl.textContent = `${i}`;
+    } else {
+      spanEl.textContent = ``;
+    }
+  }
+  // spanEl.textContent = `${i}-${alphabets[j - 1]}`;
+  cellEl.appendChild(spanEl);
+}
+
 // cellStatus에 셀 번호 전달하는 함수
 function cellClickEvent(e) {
-  const cellEl = e.target.closest(".cell");
-  console.log(cellEl);
-  const row = parseInt(cellEl.dataset.row);
-  const col = parseInt(cellEl.dataset.col);
-  const cell = spreadsheet[row][col];
-
-  e.target.textContent = `${cell.colName} - ${cell.rowName}`;
+  // const cellEl = e.target.closest(".cell");
+  // console.log(cellEl);
+  // const row = parseInt(cellEl.dataset.row);
+  // const col = parseInt(cellEl.dataset.col);
+  // const cell = spreadsheet[row][col];
+  // e.target.textContent = `${cell.colName} - ${cell.rowName}`;
 }
 
 initSpreadsheet();
