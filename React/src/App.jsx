723296513e5c8a137a5ffedb2e27455f13e3109b
asd <poo1oox@outlook.com>
2025-07-19 10:52:03 +0900

    jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./App.css";
+import Lists from "./components/Lists";
 
-function App() {
-  const todoData = [
+export default function App() {
+  const initTodoData = [
     {
       id: "1",
       title: "공부",
@@ -13,23 +15,63 @@ function App() {
       completed: false,
     },
   ];
+  const [todoData, setTodoData] = useState(initTodoData);
 
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
+  const initValue = ""; // 타이핑 기억
+  const [value, setValue] = useState(initValue);
+
+  // const btnStyle = {
+  //   color: "#fff",
+  //   border: "none",
+  //   padding: "5px 9px",
+  //   borderRadius: "50%",
+  //   cursor: "pointer",
+  //   float: "right",
+  // };
+
+  // const getStyle = (completed) => {
+  //   return {
+  //     padding: "10px",
+  //     borderBottom: "1px #ccc dotted",
+  //     textDecoration: completed ? "line-through" : "none",
+  //   };
+  // };
+
+  // const handleClick = (id) => {
+  //   console.log(id);
+  //   let newTodoData = todoData.filter((data) => data.id !== id);
+  //   console.log(newTodoData);
+  //   setTodoData(newTodoData); // 내용물을 new~로 변경
+  // };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
   };
 
-  const getStyle = () => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: "none",
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
     };
+
+    setTodoData((todoData) => [...todoData, newTodo]);
+    setValue([""]);
   };
+
+  // const handleCompletedChange = (id) => {
+  //   console.log("체크");
+  //   let newTodoData = todoData.map((data) => {
+  //     if (data.id === id) {
+  //       data.completed = !data.completed; // boolean 반전
+  //     }
+  //     return data;
+  //   });
+  //   setTodoData(newTodoData);
+  // };
 
   return (
     <div className="container">
@@ -47,16 +89,39 @@ function App() {
           청소하기
           <button style={btnStyle}>X</button>
         </div> */}
-        {todoData.map((data) => (
-          <div key={data.id} style={getStyle()}>
-            <input type="checkbox" defaultChecked={false} />
+        {/* {todoData.map((data) => (
+          <div key={data.id} style={getStyle(data.completed)}>
+            <input
+              type="checkbox"
+              onChange={() => handleCompletedChange(data.id)}
+              // defaultChecked={false}
+              checked={data.completed}
+            />
             {data.title}
-            <button style={btnStyle}>X</button>
+            <button style={btnStyle} onClick={() => handleClick(data.id)}>
+              X
+            </button>
           </div>
-        ))}
+        ))} */}
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+        {/* 폼 입력 */}
+        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="value"
+            style={{ flex: "10", padding: "5px" }}
+            placeholder="입력하기"
+            value={value}
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            value="enter"
+            className="btn"
+            style={{ flex: "1" }}
+          />
+        </form>
       </div>
     </div>
   );
 }
-
-export default App;
---
+++ React/src/components/Lists.jsx
@@ -0,0 +1,55 @@
+export default function Lists({ todoData, setTodoData }) {
+  const btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  const getStyle = (completed) => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
+
+  const handleClick = (id) => {
+    console.log(id);
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    console.log(newTodoData);
+    setTodoData(newTodoData); // 내용물을 new~로 변경
+  };
+
+  const handleCompletedChange = (id) => {
+    console.log("체크");
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed; // boolean 반전
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      {todoData.map((data) => (
+        <div key={data.id} style={getStyle(data.completed)}>
+          <input
+            type="checkbox"
+            onChange={() => handleCompletedChange(data.id)}
+            // defaultChecked={false}
+            checked={data.completed}
+          />
+          {data.title}
+          <button style={btnStyle} onClick={() => handleClick(data.id)}>
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
