7da1ac8134b408f63fe1ce3b695f6a5333f0b5b7
asd <poo1oox@outlook.com>
2025-08-20 23:28:29 +0900

    aws 테스트 위한 분리

--- react-test/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("the counter starts at 0", () => {
-  render(<App />);
-  const counterElement = screen.getByTestId("counter");
-  expect(counterElement).toHaveTextContent(0);
-});
